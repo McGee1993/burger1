0e1f541bea285db9414afc9cea3c6c785e150463
Mark McGee <mcgee11161993@gmail.com>
2019-10-20 16:37:33 -0400

    app working

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Jack Daniels Burger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
